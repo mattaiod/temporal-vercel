f8b3e2e21f40b6182dc3ce4aac610c4fe569484a
mattaiod <matteodelandhuydev@gmail.com>
2023-03-12 23:23:38 +0100

    full working linter & prettier & tsconfig

--- frontend/src/components/Test.vue
+++ frontend/src/components/Test.vue
@@ -1,11 +1,1 @@
-<script setup lang="ts">
-var rr = "Hello World"
-
-const fr = (rr: string) => {
-  return rr
-}
-
-console.log(rr)
-
-console.log(rr)
-</script>
+<script setup lang="ts"></script>
--- frontend/src/utils/strUtil.ts
+++
@@ -1,4 +0,0 @@
-export const fr = (rr: string) => {
-  return rr
-}
-console.log(3)
