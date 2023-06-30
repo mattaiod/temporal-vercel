325500909a2140c0e8554b4aa7eebe2e036ab41c
Matteo Delandhuy <matteodelandhuypro@outlook.fr>
2023-06-30 18:10:54 +0200

    functions working

--- frontend/src/modules/nhost.ts
+++ frontend/src/modules/nhost.ts
@@ -1,13 +1,13 @@
-import { NhostClient } from "@nhost/vue";
-import { createApolloClient } from "@nhost/apollo";
-import { DefaultApolloClient } from "@vue/apollo-composable";
+import { NhostClient } from "@nhost/vue"
+import { createApolloClient } from "@nhost/apollo"
+import { DefaultApolloClient } from "@vue/apollo-composable"
 // import VueApolloComponents from "@vue/apollo-components";
-import { type Session, type User } from "@nhost/hasura-auth-js";
-import { UserModule } from "~/types";
+import { type Session, type User } from "@nhost/hasura-auth-js"
+import type { UserModule } from "~/types"
 
-const backendUrl = import.meta.env.VITE_NHOST_URL;
-const region = import.meta.env.VITE_NHOST_REGION;
-const subdomain = import.meta.env.VITE_NHOST_SUBDOMAIN;
+const backendUrl = import.meta.env.VITE_NHOST_URL
+const region = import.meta.env.VITE_NHOST_REGION
+const subdomain = import.meta.env.VITE_NHOST_SUBDOMAIN
 
 export const nhost = backendUrl
   ? new NhostClient({
@@ -16,62 +16,59 @@ export const nhost = backendUrl
   : new NhostClient({
       region,
       subdomain,
-    });
+    })
 
-export const apolloClient = createApolloClient({ nhost });
+export const apolloClient = createApolloClient({ nhost })
 
-export { Session, User };
+export { Session, User }
 
 export const install: UserModule = ({ app, router }) => {
-  app.use(nhost);
-  app.provide(DefaultApolloClient, apolloClient);
+  app.use(nhost)
+  app.provide(DefaultApolloClient, apolloClient)
   // app.use(VueApolloComponents);
 
   // AUTH GUARD
   router.beforeEach(async (to, from, next) => {
     if (to.meta.requiresAuth) {
-      await nhost.auth.isAuthenticatedAsync();
-      const session = nhost.auth.getSession();
-      if (session) {
-        next();
-      } else {
-        next("/auth/login");
-      }
-    } else next();
-  });
+      await nhost.auth.isAuthenticatedAsync()
+      const session = nhost.auth.getSession()
+      if (session) next()
+      else next("/auth/login")
+    } else {
+      next()
+    }
+  })
   // NOTAUTH GUARD
   router.beforeEach(async (to, from, next) => {
     if (to.meta.requiresNotAuth) {
-      await nhost.auth.isAuthenticatedAsync();
-      const session = nhost.auth.getSession();
-      if (session) {
-        next("/profile");
-      } else {
-        next();
-      }
-    } else next();
-  });
+      await nhost.auth.isAuthenticatedAsync()
+      const session = nhost.auth.getSession()
+      if (session) next("/profile")
+      else next()
+    } else {
+      next()
+    }
+  })
   // ROLES GUARD
   router.beforeEach(async (to, from, next) => {
     if (to.meta.roles && to.meta.roles.length > 0) {
-      await nhost.auth.isAuthenticatedAsync();
-      const session = nhost.auth.getSession();
+      await nhost.auth.isAuthenticatedAsync()
+      const session = nhost.auth.getSession()
       if (session) {
-        const userRoles = session.user?.roles;
+        const userRoles = session.user?.roles
         // console.log('ROLES USER', userRoles)
         // console.log('REQUIRED ROLES', to.meta.roles)
-        let userHasRole = false;
+        let userHasRole = false
         to.meta.roles.forEach((r) => {
-          if (userRoles?.includes(r)) userHasRole = true;
-        });
-        if (userHasRole) {
-          next();
-        } else {
-          next("/");
-        }
+          if (userRoles?.includes(r)) userHasRole = true
+        })
+        if (userHasRole) next()
+        else next("/")
       } else {
-        next();
+        next()
       }
-    } else next();
-  });
-};
+    } else {
+      next()
+    }
+  })
+}
